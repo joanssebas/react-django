78611ae8cca1796fa1de36f3bcdc2e770a1492db
Joanssebas <yoandec@hotmail.com>
2022-06-28 20:51:58 -0500

    sistema de rutas

---
+++ icard_react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {ClientLayout} from "./layouts";
+import {Navigation} from "./routes";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hola mundo!</h1>
+      <Navigation />
+    </div>
+  );
+}
---
+++ icard_react/src/layouts/BasicLayout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function BasicLayout(props) {
+  const {children} = props;
+
+  return children;
+}
